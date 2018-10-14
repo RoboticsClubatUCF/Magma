cb450866ff0cd8ce4df558564eabc26e7e50080a
fsolanet5811 <36056924+fsolanet5811@users.noreply.github.com>
2018-10-14 12:51:24 -0400

    Add files via upload

---
+++ FireballWitchPumpkin/FireBallScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallScript : MonoBehaviour {
+
+    public string wallBoundaryTag;
+    public string enemyTag;
+
+    //detects collision type
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(wallBoundaryTag)) //projectile hit a wall or a boundary
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.collider.CompareTag(enemyTag))
+        {
+            handleEnemyCollision(collision.collider.gameObject);
+        }
+    }
+
+    //handles the collision if collided with an enemy
+    void handleEnemyCollision(GameObject enemy)
+    {
+        //INSERT ENEMY COLLISION CODE
+    }
+}
---
+++ FireballWitchPumpkin/FireballLaunchScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballLaunchScript : MonoBehaviour {
+
+    public GameObject fireBall;
+    public float speed;
+    public string inputAxis;
+    public float cooldownSeconds;
+    private bool canFire = true;
+    public bool isPlayer;
+
+    //For enemy only
+    public GameObject player;
+    private Vector3 direction = new Vector3();
+
+
+
+    void Update () {
+        if(isPlayer)
+            checkForLaunchInput(inputAxis);
+        else
+        {
+            aimAtPlayer();
+            checkShoot();
+        }
+
+	}
+
+    //checks to see if the fire button was pressed and attempts to fire if pressed  
+    void checkForLaunchInput(string launchAxis)
+    {
+        float fireInput = Input.GetAxis(launchAxis);
+        if(fireInput != 0)
+        {
+            attemptToFire(transform.position);
+        }
+    }
+
+    //checks to see if the player can fire the projectile and fires it if it can
+    void attemptToFire(Vector3 shotDirection)
+    {
+        if (canFire)
+            fire(shotDirection);
+    }
+
+    //fires the projectile and starts the cooldown period
+    void fire(Vector3 shotDirection)
+    {
+        GameObject toFire = Instantiate(fireBall, transform.position, transform.rotation);
+        toFire.GetComponent<Rigidbody>().velocity = Vector3.Normalize(shotDirection) * speed;
+        canFire = false;
+        Invoke("resetCooldown", cooldownSeconds);
+    }
+
+    //resets the cooldown
+    void resetCooldown()
+    {
+        canFire = true;
+    }
+
+    //sets the direction of the shot (for enemy only)
+    void aimAtPlayer()
+    {
+        direction = player.transform.position - transform.position;
+    }
+
+    //checks if the enemy sees the player and can shoot
+    void checkShoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, direction, out hit))
+        {
+            //checks if the hit object was the player (no object between launcher and the player)
+            if(hit.collider.CompareTag("Player"))
+            {
+                attemptToFire(direction);
+            }
+        }
+    }
+
+}
+
+
